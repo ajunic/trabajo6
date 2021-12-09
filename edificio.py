ea3ce17439d0bfdc5b9c630158d8745daf4fc41e
guill <gegc@dnkweb.com>
2021-12-08 23:51:25 -0600

    Agregados doble guiones

--- edificio.py
+++ edificio.py
@@ -1,27 +1,27 @@
 class Edificio:
     def __init__(self, nombre_edificio, direccion, cantidad_pisos, cantidad_aulas):
-        self.nombre_edificio = nombre_edificio
-        self.direccion = direccion
-        self.cantidad_pisos = cantidad_pisos
-        self.cantidad_aulas = cantidad_aulas
+        self.__nombre_edificio = nombre_edificio
+        self.__direccion = direccion
+        self.__cantidad_pisos = cantidad_pisos
+        self.__cantidad_aulas = cantidad_aulas
 
     #Defining Getters and Setters
     def get_nombre_edificio(self):
-        return self.nombre_edificio
+        return self.__nombre_edificio
     def set_nombre_edificio(self, nombre_edificio):
-        self.nombre_edificio == nombre_edificio
+        self.__nombre_edificio == nombre_edificio
     def get_direccion(self):
-        return self.direccion
+        return self.__direccion
     def set_direccion(self, direccion):
-        self.direccion == direccion
+        self.__direccion == direccion
     def get_cantidad_pisos(self):
-        return self.cantidad_pisos
+        return self.__cantidad_pisos
     def set_cantidad_pisos(self, cantidad_pisos):
-        self.cantidad_pisos == cantidad_pisos
+        self.__cantidad_pisos == cantidad_pisos
     def get_cantidad_aulas(self):
-        return self.cantidad_pisos
+        return self.__cantidad_pisos
     def set_cantidad_aulas(self, cantidad_aulas):
-        self.cantidad_aulas == cantidad_aulas
+        self.__cantidad_aulas == cantidad_aulas
 
     #Property definitions for each attribute.
 
